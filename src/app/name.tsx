f5cde58d8ee7e35109f5139c655e36ae2fefacff
Montek <87750128+Montekkundan@users.noreply.github.com>
2025-04-15 21:03:20 -0700

    notion x nextjs

--- src/app/name.tsx
+++ src/app/name.tsx
@@ -1,9 +1,19 @@
+import { Nextjs, Notion } from '@/components/icons';
 import { unstable_ViewTransition as ViewTransition } from 'react';
 
-export function NameTransition({name}: { name: string }) {
+export function NameTransition({ name }: { name: string }) {
   return (
     <ViewTransition>
-      <h1 className="font-medium pt-12">
+      <div className='flex items-center gap-2'>
+        <Notion className='h-10 w-10' />
+        <span>
+          <svg fill="#FFF" width="20px" height="20px" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+            <path d="M202.82861,197.17188a3.99991,3.99991,0,1,1-5.65722,5.65624L128,133.65723,58.82861,202.82812a3.99991,3.99991,0,0,1-5.65722-5.65624L122.343,128,53.17139,58.82812a3.99991,3.99991,0,0,1,5.65722-5.65624L128,122.34277l69.17139-69.17089a3.99991,3.99991,0,0,1,5.65722,5.65624L133.657,128Z" />
+          </svg>
+        </span>
+        <Nextjs className='h-10 w-10' />
+      </div>
+      <h1 className="font-medium pt-4">
         <span className="sr-only">{name}</span>
         <span
           aria-hidden="true"
